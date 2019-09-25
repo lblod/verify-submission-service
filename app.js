5531fdd51a46fefcac889e07cebe9ba4bd0980a5
Claire Lovisa <lovisa.claire@gmail.com>
2019-09-25 16:39:59 +0200

    Add endpoint to get inzendingen triples

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import { app, errorHandler } from 'mu';
 
 import {
-  fetchInzendingen
+  fetchInzendingen,
+  fetchInzendingTriples
 } from './queries';
 
 app.get('/', function( req, res ) {
@@ -16,6 +17,20 @@ app.get('/bestuurseenheid/:uri', async function( req, res ) {
       res.status(200).send({inzendingen}).end();
     } else {
       res.status(204).send({inzendingen}).end();
+    }
+  } catch (e) {
+    console.log(`An error has occured: ${e}`)
+  }
+} );
+
+app.get('/inzending/:uri', async function( req, res ) {
+  try {
+    const taskUri = req.params.uri;
+    const triples = await fetchInzendingTriples(taskUri);
+    if (triples.length > 0) {
+      res.status(200).send({triples}).end();
+    } else {
+      res.status(204).send({triples}).end();
     }
   } catch (e) {
     console.log(`An error has occured: ${e}`)
--- queries.js
+++ queries.js
@@ -12,17 +12,17 @@ async function fetchInzendingen(bestuurseenheidUri) {
     PREFIX eli: <http://data.europa.eu/eli/ontology#>
 
     CONSTRUCT {
-      ?inzending a meb:Submissions ;
-        adms:status ?inzendingStatus .
-      ?inzendingStatus skos:prefLabel ?inzendingStatusLabel .
+      ?inzending a meb:Submission ;
+        adms:status ?status .
+      ?inzendingStatus skos:prefLabel ?statusLabel .
     }
     WHERE {
       GRAPH ${sparqlEscapeUri(publicGraph)} {
-        ?inzending a meb:Submissions ;
+        ?inzending a meb:Submission ;
           pav:createdBy ${sparqlEscapeUri(bestuurseenheidUri)} ;
-          adms:status ?inzendingStatus ;
+          adms:status ?status ;
           dct:subject ?document .
-        ?inzendingStatus skos:prefLabel ?inzendingStatusLabel .
+        ?inzendingStatus skos:prefLabel ?statusLabel .
         ?document eli:date_publication ?publicationDate .
       }
     }
@@ -30,6 +30,78 @@ async function fetchInzendingen(bestuurseenheidUri) {
   `);
 }
 
+async function fetchInzendingTriples(taskUri) {
+  return await query(`
+    PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
+    PREFIX adms: <http://www.w3.org/ns/adms#>
+    PREFIX pav: <http://purl.org/pav/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX eli: <http://data.europa.eu/eli/ontology#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
+    PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+    PREFIX melding: <http://lblod.data.gift/vocabularies/automatische-melding/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX eli: <http://data.europa.eu/eli/ontology#>
+    PREFIX elod: <http://linkedeconomy.org/ontology#>
+    PREFIX lblodBesluit: <http://lblod.data.gift/vocabularies/besluit/>
+
+    CONSTRUCT {
+      ?inzending a meb:Submission ;
+        mu:uuid ?uuidInzending ;
+        pav:createdBy ?organization ;
+        pav:providedBy ?publisher ;
+        dct:subject ?submittedResource ;
+        adms:status ?status ;
+        prov:atLocation ?url ;
+        nie:hasPart ?file ;
+        melding:authenticityType ?authenticityType ;
+        dct:description ?beschrijving ;
+        rdfs:comment ?opmerking .
+      ?submittedResource a foaf:Document ;
+        mu:uuid ?uuidDocument ;
+        eli:date_publication ?publicationDate ;
+        eli:passed_by ?passedBy ;
+        eli:is_about ?subject ;
+        elod:financialYear ?reportYear ;
+        eli:first_date_entry_in_force ?firstDateInForce ;
+        eli:date_no_longer_in_force ?dateNoLongerInForce ;
+        lblodBesluit:chartOfAccount ?chartOfAccount ;
+        lblodBesluit:taxRate ?taxRate ;
+        lblodBesluit:taxType ?taxType ;
+        lblodBesluit:hasAdditionalTaxRate ?hasAdditionalTaxRate .
+    }
+    WHERE {
+      GRAPH ${sparqlEscapeUri(publicGraph)} {
+        ${sparqlEscapeUri(taskUri)} prov:generated ?inzending .
+        ?inzending a meb:Submission ;
+          mu:uuid ?uuidInzending ;
+          pav:createdBy ?organization ;
+          pav:providedBy ?publisher ;
+          dct:subject ?submittedResource ;
+          adms:status ?status ;
+          prov:atLocation ?url ;
+          nie:hasPart ?file ;
+          melding:authenticityType ?authenticityType ;
+          dct:description ?beschrijving ;
+          rdfs:comment ?opmerking .
+        ?submittedResource a foaf:Document ;
+          mu:uuid ?uuidDocument ;
+          eli:date_publication ?publicationDate ;
+          eli:passed_by ?passedBy ;
+          eli:is_about ?subject ;
+          elod:financialYear ?reportYear ;
+          eli:first_date_entry_in_force ?firstDateInForce ;
+          eli:date_no_longer_in_force ?dateNoLongerInForce ;
+          lblodBesluit:chartOfAccount ?chartOfAccount ;
+          lblodBesluit:taxRate ?taxRate ;
+          lblodBesluit:taxType ?taxType ;
+          lblodBesluit:hasAdditionalTaxRate ?hasAdditionalTaxRate .
+      }
+    }
+  `);
+}
 export {
-  fetchInzendingen
+  fetchInzendingen,
+  fetchInzendingTriples
 };
